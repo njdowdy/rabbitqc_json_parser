278298613f62131cad17f23d6e5835a6071bf931
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-04-09 19:19:12 -0700

    Changed to track more variables in a series of lists rather than just ID and read count in a dictionary.

--- script.py
+++ script.py
@@ -3,8 +3,12 @@ import os
 import re
 import csv
 
-input_dir = '/home/njdowdy/Documents/RabbitQC_Reports/TEST DATA/'  # define input directory containing json data
+input_dir = '/home/njdowdy/Documents/RabbitQC_Reports/'  # define input directory containing json data
 output = {'sequenceID': 'rawReadCount'}  # define output dictionary with header row
+sequence_ids = ['sequenceID']
+filenames = ['filename']
+raw_read_counts = ['raw_read_count']
+dates = ['date']
 
 for subdir, dirs, files in os.walk(input_dir):
     for file in files:
@@ -13,22 +17,23 @@ for subdir, dirs, files in os.walk(input_dir):
             with open(os.path.join(subdir, file), 'r') as myfile:
                 data = myfile.read()
             # parse file
-            obj = json.loads(data)
-            # print read number
-            # print("file: " + str(re.findall(r'_(I[0-9]*)_', file)[0]))
-            # print("reads: " + str(obj['summary']['before_filtering']['total_reads']))
-            # parse data
-            sequence_id = str(re.findall(r'_(I[0-9]*)_', file)[0])
-            raw_reads = int(obj['summary']['before_filtering']['total_reads'])
-            # store data
-            if sequence_id not in output:
-                output.update({sequence_id: raw_reads})
-            else:  # if sequence_id exists, the reads were split into multiple files; add them up
-                # TODO: ensure DATE is same; redos from other dates maybe should not be added together
-                # TODO: track the number of files added together; doesn't fit in dictionary though
-                output.update({sequence_id: output[sequence_id] + raw_reads})
+            if data != '':
+                obj = json.loads(data)
+                # print read number
+                # print("file: " + str(re.findall(r'_(I[0-9]*)_', file)[0]))
+                # print("reads: " + str(obj['summary']['before_filtering']['total_reads']))
+                # parse and store data
+                filenames.append(file)
+                sequence_id = str(re.findall(r'(I[0-9]*)_', file)[0])
+                sequence_ids.append(sequence_id)
+                raw_read_count = int(obj['summary']['before_filtering']['total_reads'])
+                raw_read_counts.append(raw_read_count)
+                dates.append(str(subdir.split('/')[-1]))
+            else:
+                print(f"WARNING!... File Empty: " + file)
 # write output data to csv
-with open('output/test.csv', 'w') as f:
+rows = zip(filenames, sequence_ids, dates, raw_read_counts)
+with open('output/output.csv', "w") as f:
     writer = csv.writer(f)
-    for k, v in output.items():
-        writer.writerow([k, v])
+    for row in rows:
+        writer.writerow(row)
