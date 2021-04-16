85c023f4f9158b97eec350f2a6a443e374312509
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-04-16 07:26:33 -0700

    Updated code to allow for pairing to existing assembly statistics given in 'raw_read_analysis.csv'

--- script.py
+++ script.py
@@ -2,15 +2,19 @@ import json
 import os
 import re
 import csv
+import pandas as pd
 
-input_dir = '/home/njdowdy/Documents/RabbitQC_Reports/'  # define input directory containing json data
-output = {'sequenceID': 'rawReadCount'}  # define output dictionary with header row
-sequence_ids = ['sequenceID']
-filenames = ['filename']
-raw_read_counts = ['raw_read_count']
-dates = ['date']
+external_input = '/home/njdowdy/Documents/RabbitQC_Reports/'  # define input directory containing json data
+input_read_data = 'input/raw_read_analysis.csv'
 
-for subdir, dirs, files in os.walk(input_dir):
+sequence_ids = []
+filenames = []
+total_raw_read_counts = []
+paired_raw_read_counts = []
+dates = []
+headers = ['Individual', 'filename', 'date', 'total_raw_read_count', 'paired_raw_read_count']
+
+for subdir, dirs, files in os.walk(external_input):
     for file in files:
         if '.json' in file:
             # read file
@@ -26,14 +30,34 @@ for subdir, dirs, files in os.walk(input_dir):
                 filenames.append(file)
                 sequence_id = str(re.findall(r'(I[0-9]*)_', file)[0])
                 sequence_ids.append(sequence_id)
-                raw_read_count = int(obj['summary']['before_filtering']['total_reads'])
-                raw_read_counts.append(raw_read_count)
+                total_raw_read_count = int(obj['summary']['before_filtering']['total_reads'])
+                total_raw_read_counts.append(total_raw_read_count)
+                paired_raw_read_counts.append(total_raw_read_count/2)
                 dates.append(str(subdir.split('/')[-1]))
             else:
                 print(f"WARNING!... File Empty: " + file)
+
+# convert json data to dataframe
+json_data_0 = zip(sequence_ids, filenames, dates, total_raw_read_counts, paired_raw_read_counts)
+json_data = pd.DataFrame(list(json_data_0), columns=headers)
+
 # write output data to csv
-rows = zip(filenames, sequence_ids, dates, raw_read_counts)
+rows = zip(sequence_ids, filenames, dates, total_raw_read_counts, paired_raw_read_counts)
 with open('output/output.csv', "w") as f:
     writer = csv.writer(f)
+    writer.writerow(headers)
     for row in rows:
         writer.writerow(row)
+
+# pair to assembly data
+assembly_data = pd.read_csv(input_read_data)
+
+# merge json and assembly data
+merged_data = pd.merge(assembly_data, json_data, on=['Individual'], how='right')
+
+# make new column with percent reads
+merged_data["percent_recovered_total"] = merged_data["total_raw_read_count"] / merged_data["nRawReads"]
+merged_data["percent_recovered_paired"] = merged_data["paired_raw_read_count"] / merged_data["nRawReads"]
+
+# write to csv
+merged_data.to_csv('output/merged_data.csv', header=True, index=False)
